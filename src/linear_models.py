896ac15786b71fe4ae0d196ae4edd43660479b60
jgoerner <joshua.goerner@gmail.com>
2018-01-14 14:47:04 +0100

    finished bayes regressor w/ evidence approximation

--- src/linear_models.py
+++ src/linear_models.py
@@ -1,8 +1,70 @@
-# %load src/linear_models.py
-# %load ./src/linear_models.py
 import numpy as np
 import pandas as pd
+from sklearn.base import BaseEstimator, RegressorMixin
+from scipy import stats
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+
+########## REGRESSOR ##########
+from sklearn.base import BaseEstimator, RegressorMixin
+from scipy import stats
+class BayesRegressor(BaseEstimator, RegressorMixin):
+    
+    def __init__(self, phi, a, b_std, M0=None):
+        # misc attributes
+        ndim = len(phi)
+        self.phi = phi
+        self.b = 1. / (b_std * b_std)
+        self._seen_samples = 0
+        
+        if not M0:
+            M0 = stats.norm.rvs(size=ndim)
+        # update belief
+        self._update_belief(
+            np.matrix(M0).T,
+            np.matrix(1./a*np.identity(ndim)),
+        )
+    
+    def predict(self, X):
+        DM = np.matrix(build_design_matrix(X, self.phi))
+        mu = np.squeeze(np.asarray(self.M_prior.T*DM.T))
+        sig = np.squeeze(np.asarray(np.sqrt(np.diag(DM*self.S_prior*DM.T))))
+        return mu, sig
+    
+    def fit(self, X, Y):
+        S_post = self._update_S(X)
+        M_post = self._update_M(X, Y)
+        self.b = self._update_beta(X, Y)
+        self._update_belief(M_post, S_post)
+    
+    def _update_belief(self, M_post, S_post):
+        self.M_prior = M_post
+        self.S_prior = S_post
+        self.prior = stats.multivariate_normal(
+            np.squeeze(np.asarray(self.M_prior)), 
+            self.S_prior,
+        )
+        return True
+    
+    def _update_beta(self, X, Y):
+        DM = np.matrix(build_design_matrix(X, self.phi))
+        self._seen_samples += 1
+        return self._seen_samples / np.linalg.norm(np.matrix(Y).T - DM*self.M_prior)
+    
+    def _update_S(self, X):
+        DM = np.matrix(build_design_matrix(X, self.phi))
+        return np.linalg.inv(np.linalg.inv(self.S_prior) + self.b*DM.T*DM)
+    
+    def _update_M(self, X, Y):
+        # get design matrix
+        DM = np.matrix(build_design_matrix(X, self.phi))
+        # reshape Y
+        Y_ = np.matrix(np.atleast_2d(Y).reshape(-1, 1))
+        # return M_post
+        return self._update_S(X) * (np.linalg.inv(self.S_prior)*self.M_prior + self.b*DM.T*Y_)
+    
+########## UTILS ##########
 def build_design_matrix(X, PHI):
     """Build the design matrix
     
@@ -60,3 +122,33 @@ def maximum_likelihood_regression(X, PHI, Y, reg=0):
     # compute parameter
     WM = np.linalg.inv(DMM.T*DM+LM)*DMM.T*YM
     return np.squeeze(np.asarray(WM))
+
+def plot_posterior(br, x_min, x_max):
+    x = np.linspace(x_min, x_max, 100)
+    mu, sig = br.predict(x)
+    plt.plot(x, mu)
+    plt.fill_between(
+        x,
+        mu - 3*sig,
+        mu + 3*sig,
+        alpha=.25,
+    );
+    
+    
+def plot_belief(br, lower, upper, resolution=100):
+    xx, yy = np.meshgrid(
+        np.linspace(lower, upper, resolution),
+        np.linspace(lower, upper, resolution)
+    )
+    zz = br.prior.pdf(np.array([xx.ravel(), yy.ravel()]).T).reshape(xx.shape)
+    plt.imshow(zz, cmap="inferno")
+    plt.xticks(np.linspace(0, resolution,len(np.arange(lower, upper+.5, .5))), 
+               map(str, np.arange(lower, upper+.5, .5)), 
+               rotation=45,
+               ha="right",
+              )
+    plt.yticks(np.linspace(0, resolution,len(np.arange(lower, upper+.5, .5))), 
+               map(str, np.arange(lower, upper+.5, .5)), 
+               rotation=45,
+               ha="right",
+              )
